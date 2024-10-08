1154c66d838bd53aaf857abd382035d71549816e
Elo <Elo@LAPTOP-K5V7MDVB>
2024-10-08 12:34:07 +0200

    Update 1.0

--- CodiumTask/Program.cs
+++ CodiumTask/Program.cs
@@ -4,6 +4,7 @@ using CodiumTask.Components;
 using Repository.Data;
 using Serilog;
 using Repository.Services;
+using Microsoft.Extensions.Configuration;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
@@ -23,7 +24,7 @@ builder.Services.AddControllers();
 builder.Services.AddHttpClient();
 
 builder.Services.AddDbContext<DataContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<EmployeeService>();
 builder.Services.AddScoped<PositionService>();
@@ -40,7 +41,8 @@ app.Use(async (context, next) =>
     catch (Exception ex)
     {
         Log.Error(ex, "[Server] An unhandled exception occurred.");
-        throw;
+        context.Response.StatusCode = 500;
+        await context.Response.WriteAsync("A server error occurred.");
     }
 });
 
---
+++ Repository/Migrations/20241008094425_InitialCreate.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Positions",
+                columns: table => new
+                {
+                    PositionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PositionName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Positions", x => x.PositionID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PositionID = table.Column<int>(type: "int", nullable: true),
+                    IPAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IPCountryCode = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.EmployeeID);
+                    table.ForeignKey(
+                        name: "FK_Employees_Positions_PositionID",
+                        column: x => x.PositionID,
+                        principalTable: "Positions",
+                        principalColumn: "PositionID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_PositionID",
+                table: "Employees",
+                column: "PositionID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Positions");
+        }
+    }
+}
